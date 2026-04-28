2298c120d9c06af5e494f56ad14310e5e7a6af1f
wwwabishan141-gif <www.abishan141@gmail.com>
2026-04-28 11:56:32 +0530

    Add files via upload

--- lib/products.ts
+++ lib/products.ts
@@ -147,6 +147,22 @@ export const products: Product[] = [
       detail: "/dtf-007-detail.png",
     },
   },
+  {
+    id: "tee-008",
+    name: "Vintage Speed Beetle Graphic Tee",
+    price: 1750,
+    category: "tshirts",
+    designThemes: ["minimalist", "typography"],
+    colors: ["White", "Black", "Grey"],
+    sizes: ["XS", "S", "M", "L", "XL", "XXL"],
+    description: "Car print t-shirts Clean lines, simple design, maximum impact. The everyday minimalist tee Round Neck Regular Fit.",
+    image: "/dtf-008-front.png",
+    images: {
+      front: "/dtf-008-front.png",
+      back: "/dtf-008-back.png",
+      detail: "/dtf-008-detail.png",
+    },
+  },
 ]
 
 export function getProductById(id: string): Product | undefined {
